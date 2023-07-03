fc4896197726bffa58dc659fc70fd8a3f7b17d17
Zero <79031203+Zerosfragile@users.noreply.github.com>
2023-07-03 19:54:33 -0400

    layout style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import BtnContainer from '@/components/btn-container';
+import BtnExpandable from '@/components/btn-container/btn-expandable';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="h-full w-full overflow-y-hidden">
+          <div
+            className={`bg-black h-[calc(100vh-129px)] overflow-x-hidden text-center relative hud-border`}
+          >
+            {children}
+          </div>
+          <div
+            className={`flex justify-between items-center h-[75px] bottom-0 text-center hud-border`}
+          >
+            <BtnContainer>
+              <BtnExpandable 
+                defaultLabel={'TEST'} 
+                onClick={function (): void {
+                  throw new Error('Function not implemented.');
+                } }
+              >
+              </BtnExpandable>
+            </BtnContainer>
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
