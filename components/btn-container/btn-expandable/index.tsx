e4e633ac4ebe0952f43272c3aad2a06d4bb5041e
Zero <79031203+Zerosfragile@users.noreply.github.com>
2023-07-03 19:55:14 -0400

    btnexpandable init / style

---
+++ components/btn-container/btn-expandable/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+type Props = {
+  children?: React.ReactNode;
+  prefix?: string;
+  threshold?: number;
+  labels?: {
+    breakpoint: number;
+    text: string;
+  };
+  defaultLabel: string;
+  onClick(): void;
+};
+
+const BtnExpandable = (props: Props) => {
+  const { children } = props;
+ 
+    return (
+      <button className="text-OffWhite font-[CygnitoMono-011] uppercase font-normal text-size-auto text-[11.25px] hover:shadow-inset hover:border-LunarGrey-light/[.4] hover:text-OffWhite-light transition-all duration-500 bg-LunarGrey-darkest/[.9] rounded-[6px] pt-[9.75px] pb-[7.5px] px-[18px] border border-solid border-OffWhite/[0] my-0 mx-[11.25px] max-h-45">
+        test
+      </button>
+    );
+};
+
+export default BtnExpandable;
