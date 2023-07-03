ac7ffefda5beca1c731cab845ead7a702ff13ba9
Zero <79031203+Zerosfragile@users.noreply.github.com>
2023-07-03 19:54:42 -0400

    layout style

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
 import Image from 'next/image'
 
 export default function Home() {
-  const border = "border border-[#e2e2e233]";
-  const hud = "w-auto m-[18px] rounded-[18.75px]";
   return (
-    <div className="h-full w-full overflow-y-hidden">
-      <div
-        className={`bg-black h-[calc(100vh-129px)] overflow-x-hidden text-center relative ${border} ${hud}`}
-      >
-        
-      </div>
-      <div
-        className={`flex justify-between items-center h-[75px] bottom-0 text-center ${border} ${hud}`}
-      ></div>
-    </div>
+    <div>hello</div>
   );
 }
