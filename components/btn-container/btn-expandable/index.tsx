45d4521f2154b8a2e82f70dc341f7bd13f08c4db
Zero <79031203+Zerosfragile@users.noreply.github.com>
2023-07-03 21:33:59 -0400

    useIndexPrefix hook

--- components/btn-container/btn-expandable/index.tsx
+++ components/btn-container/btn-expandable/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-type Props = {
+export type Props = {
   children?: React.ReactNode;
   prefix?: string;
   threshold?: number;
---
+++ components/btn-container/hooks/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const useIndexPrefix = (children: any) => {
+    let index = 0;
+    const addIndexPrefix = () => {
+      // Function to add the prefix prop if it doesn't exist
+      const addPrefix = (
+        child: React.DetailedReactHTMLElement<any, HTMLElement>
+      ) => {
+        if (!child.props.prefix) {
+          const newProps = {
+            ...child.props,
+            prefix: String(index + 1).padStart(2, "0") + " //",
+          };
+          return React.cloneElement(child, newProps);
+        }
+
+        return child;
+      };
+
+      // Map over the children and apply the index prefix if needed
+      const childrenWithPrefix = React.Children.map(children, (child) => {
+        index++;
+        return addPrefix(child);
+      });
+
+      return childrenWithPrefix;
+    };
+
+    return addIndexPrefix();
+};
+
+
