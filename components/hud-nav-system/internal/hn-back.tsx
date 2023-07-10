8b34ce0da313ecb8aff77b2d2eaf1f3a2d17be37
Zero <79031203+Zerosfragile@users.noreply.github.com>
2023-07-10 15:51:45 -0400

    site link pointer cursor

--- components/hud-nav-system/internal/hn-back.tsx
+++ components/hud-nav-system/internal/hn-back.tsx
@@ -37,7 +37,7 @@ const HNBack = ({ controls, handleMouseLeave }: Props) => {
             transition: { duration: 0.1 },
           }}
           onClick={() => handleClick(item.route, item.event)}
-          className="text-decoration-none w-[100% - 15px] m-[7.5px] inline-block truncate rounded-[6px] bg-LunarGrey-darkest/[.5] px-[14.5px] pb-[5.5px] pt-[7px] font-[CygnitoMono-011] text-[11px] font-normal uppercase leading-extra-tight text-OffWhite/[.66] opacity-100 shadow-LunarGrey-light/[.4] transition-all duration-500 ease-cubic hover:bg-LunarGrey-dark/[.40] hover:text-OffWhite-light hover:shadow-glow"
+          className="text-decoration-none w-[100% - 15px] m-[7.5px] inline-block cursor-pointer truncate rounded-[6px] bg-LunarGrey-darkest/[.5] px-[14.5px] pb-[5.5px] pt-[7px] font-[CygnitoMono-011] text-[11px] font-normal uppercase leading-extra-tight text-OffWhite/[.66] opacity-100 shadow-LunarGrey-light/[.4] transition-all duration-500 ease-cubic hover:bg-LunarGrey-dark/[.40] hover:text-OffWhite-light hover:shadow-glow"
         >
           {item.title}
         </motion.a>
