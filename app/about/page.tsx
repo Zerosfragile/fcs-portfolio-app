678f2d1cd710f1d27fa71f67b759aafa11c44bbd
Zero <writefragile@gmail.com>
2023-09-03 19:52:05 -0400

    backlink about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,17 +9,32 @@ export default function About() {
   const [loading, setLoading] = useState(false); 
   if (loading) return <Initializing/>;
   return (
-    <div
-    className="
-      bg-black hud-border ease relative flex h-[calc(100vh-39px)] justify-center overflow-x-hidden
-      text-center duration-500 ease-cubic
-    "
-  >
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 group">
-      <div className="font-[CygnitoMono-011] text-[11.25px] font-normal uppercase text-OffWhite/[.33] transition-all duration-500 ease-linear group-hover:text-OffWhite/[.66]">
-        <motion.p initial={{opacity:0, y: -100}} animate={{opacity: 100, y:0}} transition={{duration: 2.5}}>To get something you never had</motion.p>
-        <motion.p initial={{opacity:0}} animate={{opacity: 100, y:0}} transition={{duration: 1, delay: 2, ease: "easeInOut"}}>You Have to do something you have never done.</motion.p>
-      </div></div>
+      <div
+      className="
+        bg-black hud-border ease relative flex h-[calc(100vh-39px)] justify-center overflow-x-hidden
+        text-center duration-500 ease-cubic
+      "
+    >
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 group">
+        <motion.div initial={{opacity:0}} animate={{opacity: 100, y:0}} transition={{duration: 1, delay: 3.5, ease: "easeInOut"}}>
+          <Link
+              href={"/"}
+              className="  flex flex-col items-center justify-center m-6"
+            >
+              <Image
+                src={"/images/056-Modern_Icons.png"}
+                alt={""}
+                width={75}
+                height={75}
+                className="opacity-10 invert transition-all duration-500 ease-linear group-hover:opacity-50"
+              />
+              </Link>
+        </motion.div>
+        <div className="font-[CygnitoMono-011] text-[11.25px] font-normal uppercase text-OffWhite/[.33] transition-all duration-500 ease-linear group-hover:text-OffWhite/[.66]">
+          <motion.p initial={{opacity:0, y: -100}} animate={{opacity: 100, y:0}} transition={{duration: 2.5}}>To get something you never had</motion.p>
+          <motion.p initial={{opacity:0}} animate={{opacity: 100, y:0}} transition={{duration: 1, delay: 2, ease: "easeInOut"}}>You Have to do something you have never done.</motion.p>
+        </div>
+      </div>
     </div>
   );
 }
