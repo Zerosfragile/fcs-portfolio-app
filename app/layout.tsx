7175a97d8911613440cdb9186ebae9d109ae0c91
Zero <writemarcuslim@gmail.com>
2023-09-23 22:54:09 -0400

    Socials Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="h-full w-full overflow-y-hidden">{children}</div>
+        <Toaster />
       </body>
     </html>
   );
