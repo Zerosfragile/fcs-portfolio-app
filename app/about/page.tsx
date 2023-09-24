99455775227522c9c7a5b984ead600bbdd2a92db
Zero <writemarcuslim@gmail.com>
2023-09-23 22:00:45 -0400

    hide about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -262,23 +262,23 @@ const QuoteInitializing = ({
 }) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 group">
-      {/* <Link href={"/"}> */}
-      <motion.button
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 100, y: 0 }}
-        transition={{ duration: 1, delay: 3.5, ease: "easeInOut" }}
-        onClick={() => setInit(true)} //! REMOVE When Finished
-        className="flex flex-col items-center justify-center p-6 w-full"
-      >
-        <Image
-          src={"/images/056-Modern_Icons.png"}
-          alt={""}
-          width={75}
-          height={75}
-          className="opacity-10 invert transition-all duration-500 ease-linear group-hover:opacity-50"
-        />
-      </motion.button>
-      {/* </Link> */}
+      <Link href={"/"}>
+        <motion.button
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 100, y: 0 }}
+          transition={{ duration: 1, delay: 3.5, ease: "easeInOut" }}
+          // onClick={() => setInit(true)} //! REMOVE When Finished
+          className="flex flex-col items-center justify-center p-6 w-full"
+        >
+          <Image
+            src={"/images/056-Modern_Icons.png"}
+            alt={""}
+            width={75}
+            height={75}
+            className="opacity-10 invert transition-all duration-500 ease-linear group-hover:opacity-50"
+          />
+        </motion.button>
+      </Link>
       <div className="font-[CygnitoMono-011] text-[11.25px] font-normal uppercase text-OffWhite/[.33] transition-all duration-500 ease-linear group-hover:text-OffWhite/[.66] text-center min-w-[90vw]">
         <motion.p
           initial={{ opacity: 0, y: -100 }}
