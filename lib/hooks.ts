2e0ebdafad249816f3625caa6a2858f940b92b31
Zero <writemarcuslim@gmail.com>
2024-03-25 22:07:43 -0400

    feat(hooks): media query

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -73,4 +73,20 @@ export function useRotatingString(
   return currentString;
 }
 
-export default useRotatingString;
+export function useMediaQuery(query: string) {
+  const [value, setValue] = useState(false);
+
+  useEffect(() => {
+    function onChange(event: MediaQueryListEvent) {
+      setValue(event.matches);
+    }
+
+    const result = matchMedia(query);
+    result.addEventListener("change", onChange);
+    setValue(result.matches);
+
+    return () => result.removeEventListener("change", onChange);
+  }, [query]);
+
+  return value;
+}
