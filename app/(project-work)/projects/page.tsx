7b9c84cef8651ddf9340273cb43036d71f6e66cc
Zero <writemarcuslim@gmail.com>
2024-04-04 09:29:35 -0400

    feat(hudnav): rm samepage nav

--- app/(project-work)/projects/page.tsx
+++ app/(project-work)/projects/page.tsx
@@ -170,67 +170,19 @@ const HudNav = ({ eventHandlers }: { eventHandlers: EventHandlers }) => {
           ]}
         />
         <HUDN.btn
-          defaultLabel="Projects"
-          route="/projects"
-          sites={[
-            {
-              title: "Ascii-Hud",
-              route: "https://fragileservices.com",
-            },
-            {
-              title: "Inspiration Vault",
-              route: "/vault",
-            },
-            // {
-            //   title: "Playground",
-            //   route: "/Projects/playground",
-            // },
-            {
-              title: "More",
-              route: "/projects",
-            },
+          labels={[
+            { breakpoint: 350, text: "UI" },
+            { breakpoint: 850, text: "Playground" },
           ]}
+          defaultLabel="UI Playground"
+          route="/playground"
+          className="md:hidden"
         />
+
         <HUDN.btn
-          defaultLabel="Resume"
-          route="/about/resume"
-          sites={[
-            {
-              title: "PDF View",
-              route: "/about/resume/pdf",
-            },
-            {
-              title: "More",
-              route: "/about/resume",
-            },
-          ]}
-          className="max-md:hidden"
-        />
-        <HUDN.btn
-          prefix={{
-            breakpoint: 1100,
-            text: "04 // ",
-          }}
-          defaultLabel="Contact"
-          route="/contact"
-          sites={[
-            {
-              title: "Email",
-              event: "showEmail",
-            },
-            {
-              title: "Github",
-              route: "https://github.com/zerofcs",
-            },
-            {
-              title: "Linkedin",
-              route: "https://www.linkedin.com/in/marcus-lim-b6a721260/",
-            },
-            {
-              title: "More",
-              route: "/contact",
-            },
-          ]}
+          labels={[{ breakpoint: 850, text: "Vault" }]}
+          defaultLabel="Inspiration Vault"
+          route="/vault"
           className="md:hidden"
         />
       </HUDN.container>
@@ -267,9 +219,18 @@ const HudNav = ({ eventHandlers }: { eventHandlers: EventHandlers }) => {
             breakpoint: 1100,
             text: "05 // ",
           }}
-          defaultLabel="Refresh"
-          event="refresh"
-          sites={[]}
+          defaultLabel="Resume"
+          route="/about/resume"
+          sites={[
+            {
+              title: "PDF View",
+              route: "/about/resume/pdf",
+            },
+            {
+              title: "More",
+              route: "/about/resume",
+            },
+          ]}
           className="max-md:hidden"
         />
       </HUDN.container>
