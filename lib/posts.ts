92f3592487095fddad87a62c4d6bbe62b966693f
Zero <writemarcuslim@gmail.com>
2024-04-15 16:00:19 -0400

    refactor(posts util): ignore ob folder

--- lib/posts.ts
+++ lib/posts.ts
@@ -40,7 +40,7 @@ export const getBlogData = async (
 
     for (const folder of postFolders) {
       if (folder !== ".DS_Store") {
-        if (folder == "refactor") {
+        if (folder == "refactor" || folder == ".obsidian") {
           continue;
         }
         const currentDirectory = path.join(postsDirectory, folder);
