f4aa0c1e5062b4cdea69ea9ffeab3a8327eb7e04
Zero <writefragile@gmail.com>
2024-04-28 15:27:24 -0400

    fix(vault): tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
   ],
   theme: {
     colors: {
