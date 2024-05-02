451c859ead14cde3656bf93bbaa611d9d6fba209
Zero <writemarcuslim@gmail.com>
2024-05-02 17:19:26 -0400

    chore(test): misc tests

--- app/test/2/page.tsx
+++ app/test/2/page.tsx
@@ -1,143 +1,155 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {
+  AnimatePresence,
   cubicBezier,
   motion,
+  useMotionTemplate,
+  useMotionValueEvent,
   useScroll,
   useTransform,
   Variant,
   Variants,
 } from "framer-motion";
 import Image from "next/image";
+import { trace } from "console";
+import { TypingLabel } from "@/components/hud-nav-system";
 
 type Props = {};
 
 export default function Page({}: Props) {
-  const ref = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-  });
-
-  const containerWidth = useTransform(scrollYProgress, [0, 1], [576, 551], {
-    ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
-  });
-  const containerHeight = useTransform(scrollYProgress, [0, 1], [900, 68], {
-    ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
-  });
-
-  const imageWidth = useTransform(
-    scrollYProgress,
-    [0, 0.25, 1],
-    [544, 100, 48],
-    {
-      ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
-    }
-  );
-  const imageTop = useTransform(
-    scrollYProgress,
-    [0, 0.25, 0.75, 1],
-    [348, 250, 190, 4],
-    {
-      ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
-    }
-  );
-  const imageX = useTransform(scrollYProgress, [0, 1], [0, -16], {
-    ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
-  });
-  const imageRight = useTransform(scrollYProgress, [0, 1], [16, 0], {
-    ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
-  });
-
-  const textY = useTransform(scrollYProgress, [0, 0.5], [0, -150], {
-    ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
-  });
-  const textOpacity = useTransform(scrollYProgress, [0, 0.5], [1, 0], {
-    ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
-  });
-
-  useEffect(() => {
-    console.log(containerWidth, scrollYProgress);
-  }, [containerWidth, scrollYProgress]);
-
-  const variants: Variants = {
-    hidden: { opacity: 0, scale: 0 },
-    visible: { opacity: 1, scale: 1 },
-  };
-
   return (
-    <div className="flex flex-col bg-gradient-earth h-[200vh]" ref={ref}>
-      <div className="flex flex-col items-center justify-center w-full my-4 sticky top-0 translate-y-1/2">
-        <motion.div
-          style={{ width: containerWidth, height: containerHeight }}
-          animate={{
-            placeItems: "flex-start",
-          }}
-          transition={{ duration: 1, delay: 1 }}
-          className="bg-VoidBlack p-4 rounded-md shadow-lg grid place-items-center relative"
-          layout
-        >
-          <section className="max-w-xl font-mono text-OffWhite-dark flex flex-col gap-4">
-            <motion.div className="flex justify-between items-center">
-              <motion.div className="flex flex-col">
-                <motion.span className="text-sm" key="tag">
-                  @zerofcs
-                </motion.span>
-                <motion.span
-                  className="text-xs text-muted-foreground"
-                  key="name"
-                >
-                  Faustian Services
-                </motion.span>
-              </motion.div>
-              <div className="relative w-10 h-10">
-                <motion.span
-                  style={{ y: textY, opacity: textOpacity }}
-                  className="text-lg text-OffWhite absolute"
-                  key="news"
-                >
-                  News
-                </motion.span>
-              </div>
-            </motion.div>
+    <div className="flex flex-col bg-gradient-earth h-[200vh] relative">
+      <main className="h-screen flex flex-col items-center fixed top-0 left-1/2 -translate-x-1/2 gap-4 py-4 w-full max-w-[90vw] lg:max-w-4xl  ">
+        <section className="bg-VoidBlack p-4 rounded-md shadow-lg font-mono text-OffWhite-dark flex flex-col gap-4  relative overflow-hidden h-[68px] w-full">
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col">
+              <span className="text-sm" key="tag">
+                @zerofcs
+              </span>
+              <span className="text-xs text-muted-foreground" key="name">
+                Faustian Services
+              </span>
+            </div>
+          </div>
+          <div className="absolute right-4 bottom-1 min-h-[60px] max-h-[calc(100%-32px)] min-w-[48px] max-w-[calc(100%-32px)]">
+            <Image
+              src={"/images/test-preview-002.jpg"}
+              alt={"art by @vinne.art"}
+              layout="fill"
+              className="object-cover rounded-sm"
+            />
+          </div>
+        </section>
 
-            <motion.p
-              style={{ y: textY, opacity: textOpacity }}
-              key="description"
-            >
-              {`Νὴ τὸν Ἄγνωστον, ἐν τῇ ἐπωνυμίᾳ τοῦ ἀόρατου μονοπατιοῦ, ὁρίζομεν
-                τὴν ὕπαρξίν μας. Αἰνιγματώδης ὁ Ἄγνωστος Θεὸς, ὄχι ὡς τὸ ἄκρον
-                ἄωτον τῆς γνώσεως, ἀλλ' ὡς ἡ ὑπογραφή τῆς ἐπιστημονικῆς
-                ταπεινώσεως: τὸ ὅριο τῆς ἐπιγνώσεώς μας εἶναι τὸ ἀπέραντο
-                ἀγνωστικό μας. Προσλαμβάνοντας τὴν ἀγνωσία ὡς τὸν καμβὰ τῆς
-                σοφίας, καλούμαστε νὰ περπατήσουμε τὴν ἀβέβαιη ἀλήθεια τῆς
-                ἀνθρώπινης φύσεως, ἀναζητώντας τὸ φῶς μέσα ἀπὸ τὴν σκιά.
-                Ἐνστερνίζου τὸν Ἄγνωστον, γίνου μάρτυρας τῆς ἀτέλειας, ὑποδέξου
-                τὴν ἀληθινὴ σοφία ποὺ κρύβεται μέσα ἀπὸ τὸ μυστήριο. γενέσθω Amor
-                Fati, Agnōstos Theos.`}
-            </motion.p>
-            <motion.div
-              layoutId="header"
-              key="header-image"
-              style={{
-                width: imageWidth,
-
-                top: imageTop,
-                x: imageX,
-                right: imageRight,
-              }}
-              className="absolute"
-            >
-              <Image
-                src={"/images/test-preview-002.jpg"}
-                alt={"art by @vinne.art"}
-                width={544}
-                height={535}
-                className="max-h-[535px] object-cover rounded-sm"
-              />
-            </motion.div>
-          </section>
-        </motion.div>
-      </div>
+        <section className="flex-1 bg-VoidBlack w-full p-4 rounded-md shadow-lg grid place-items-center relative overflow-hidden">
+          <div className="flex flex-col p-2 w-full h-full">
+            <div>
+              <h2 className="text-OffWhite font-mono font-black text-2xl uppercase tracking-widest">
+                Featured Projects
+              </h2>
+              <span></span>
+            </div>
+            <div className="flex flex-col gap-2 py-4">
+              <AnimatePresence>
+                <CardTest
+                  img="/posts/projects/preview-musou.png"
+                  title="Musou"
+                  tags={["Game", "Unity"]}
+                  description="A game project"
+                  date="2021"
+                />
+                <CardTest
+                  img="/posts/projects/preview-ascii-hud.png"
+                  title="Ascii Hud"
+                  tags={["Game", "Unity"]}
+                  description="A game project"
+                  date="2021"
+                />
+                <CardTest
+                  img="/posts/projects/preview-ascii-hud.png"
+                  title="Ascii Hud"
+                  tags={["Game", "Unity"]}
+                  description="A game project"
+                  date="2021"
+                />
+                <CardTest
+                  img="/posts/projects/preview-ascii-hud.png"
+                  title="Ascii Hud"
+                  tags={["Game", "Unity"]}
+                  description="A game project"
+                  date="2021"
+                />
+              </AnimatePresence>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
+  );
+}
+
+function CardTest({
+  img,
+  title,
+  tags,
+  description,
+  date,
+}: {
+  img: string;
+  title: string;
+  tags: string[];
+  description: string;
+  date: string;
+}) {
+  return (
+    <motion.div
+      key={title}
+      exit={"hidden"}
+      transition={{
+        delay: 0,
+        duration: 0.5,
+        ease: "easeOut",
+      }}
+      layoutId={title}
+      initial="hidden"
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          delay: 1,
+          duration: 0.5,
+          ease: "easeOut",
+        },
+      }}
+      viewport={{ once: true }}
+      variants={{
+        hidden: { opacity: 0, y: -50 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      className="flex flex-col p-2 gap-2 rounded-lg font-mono border border-dashed border-[#d4ed31]/25"
+    >
+      <span>{title}</span>
+      <div className="grid grid-cols-2 gap-2 text-xs text-OffWhite/90  border-t border-dashed border-[#d4ed31]/25 py-2">
+        <div className="flex flex-col">
+          <span className="font-light">Year</span>
+          <span className="text-OffWhite/75">{date}</span>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-light">Tags</span>
+          <span className="text-OffWhite/75">{tags.join(", ")}</span>
+        </div>
+
+        <p className="col-span-2 font-light text-OffWhite">{description}</p>
+      </div>
+      <div className="w-full min-h-[500px] h-full flex-1 flex rounded-lg overflow-hidden border relative">
+        <Image
+          src={img}
+          alt={title + " preview"}
+          layout="fill"
+          className="object-fit rounded-sm"
+        />
+      </div>
+    </motion.div>
   );
 }
